3bfbf58b0ca88f80c97a676f71b841771d526c9b
Rishabh Anand <rishabhanand42@gmail.com>
2021-05-05 04:34:20 +0530

    a prettier change in index.ts A formatter change 	modified:   rest-api/index.ts

--- rest-api/index.ts
+++ rest-api/index.ts
@@ -4,7 +4,10 @@ import buildApp from "./app";
 dotenv.config();
 
 (async () => {
-  const connectionURI = process.env.NODE_ENV === "production" ? process.env.MONGO_URI_STRING_PROD || "" : process.env.MONGO_URI_STRING_DEV || "";
+  const connectionURI =
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGO_URI_STRING_PROD || ""
+      : process.env.MONGO_URI_STRING_DEV || "";
   const PORT = process.env.NODE_ENV === "production" ? 4500 : 5000;
   const app = await buildApp(connectionURI);
 
