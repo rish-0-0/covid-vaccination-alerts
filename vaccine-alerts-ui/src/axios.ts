f49d26f80349e5a3acc8eeebf809a52d38fb971b
Rishabh Anand <rishab@caarya.in>
2021-05-06 17:16:12 +0530

    setting up payments Setup AXIOS and SETUP Razorpay 	new file:   src/axios.ts 	modified:   src/components/payment/Payment.tsx

---
+++ vaccine-alerts-ui/src/axios.ts
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+const GOV_API = axios.create({ baseURL: "https://cdn-api.co-vin.in/api" });
--- vaccine-alerts-ui/src/components/payment/Payment.tsx
+++ vaccine-alerts-ui/src/components/payment/Payment.tsx
@@ -1,7 +1,36 @@
 import * as React from "react";
 
+function loadScript(src: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = () => {
+      resolve();
+    };
+    script.onerror = () => {
+      reject(new Error(`Error loading script: ${src}`));
+    };
+    document.body.appendChild(script);
+  });
+}
+/**
+ * 
+ * @param amount number in PAISA or smallest unit
+ * @param currency CURRENCY CODE: INR or USD etc
+ * @return boolean
+ */
+async function openRazorpayModal(amount: number, currency: "INR"): Promise<boolean> {
+  try {
+    await loadScript("https://checkout.razorpay.com/v1/checkout.js");
+    return true;
+  } catch (e) {
+    console.error("Error ocurred while opening razorpay modal\n", e);
+    return false;
+  }
+}
+
 function PaymentModal() {
-  return(
+  return (
     <div className="payment-modal">
       <h1>Help!</h1>
     </div>
