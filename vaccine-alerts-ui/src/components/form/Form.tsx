882fa3d1ff55614382e49f1af45670257e83fb1e
Rishabh Anand <rishab@caarya.in>
2021-05-07 04:50:53 +0530

    service worker shenanigans Fun with service workers 	modified:   package.json 	modified:   src/App.test.tsx 	modified:   src/components/form/Form.tsx 	modified:   src/constants.ts 	modified:   src/index.tsx 	modified:   src/service-worker.ts 	modified:   src/serviceWorkerRegistration.ts 	modified:   yarn.lock

--- vaccine-alerts-ui/src/components/form/Form.tsx
+++ vaccine-alerts-ui/src/components/form/Form.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
+import { getNotificationToken } from "../../constants";
 
 function Form() {
+  React.useEffect(() => {
+    (async () => {
+      const token = await getNotificationToken();
+      console.log("TOKEN", token);
+    })();
+  }, []);
   return (
     <div className="form-container">
-      <form id="alert-form">
-        
-      </form>
+      <form id="alert-form"></form>
     </div>
   );
 }
--- vaccine-alerts-ui/src/constants.ts
+++ vaccine-alerts-ui/src/constants.ts
@@ -5,3 +5,28 @@ export const useGlobalStyles = makeStyles((theme) => ({
     margin: theme.spacing(1),
   },
 }));
+
+function messageSW(sw: ServiceWorker, data: { type: string }) {
+  return new Promise((resolve) => {
+    const messageChannel = new MessageChannel();
+    messageChannel.port1.onmessage = (event) => {
+      resolve(event.data);
+    };
+    sw.postMessage(data, [messageChannel.port2]);
+  });
+}
+export { messageSW };
+
+export async function getNotificationToken(): Promise<any> {
+  try {
+    const sw = (await navigator.serviceWorker?.ready).active;
+    if (sw === null) throw new Error("No service worker registered :(");
+    const token = await messageSW(sw, {
+      type: "GET_NOTIFICATION_TOKEN",
+    });
+    return token;
+  } catch (e) {
+    console.error("Error ocurred while getting notification token\n", e);
+    return null;
+  }
+}
